e3ed0bace7dfd526b114aa2643c431de10d0dd06
AIE\s162866 <contact@trentwilliamswanson.com>
2018-03-19 16:07:21 +1100

    Added sowrd blendshapes and hammer animation

---
+++ BlackSmithSim/Assets/Scripts/Hammer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hammer : MonoBehaviour {
+
+	Camera camera;
+
+	Animator hitAnimation;
+	Transform hammerPos;
+
+	void Start() {
+		camera = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera>();
+		hammerPos = transform.parent.transform;
+		hitAnimation = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+		if (Physics.Raycast(ray, out hit)) {
+			if (hit.transform.tag == "Table") {
+				Transform objectHit = hit.transform;
+				hammerPos.position = new Vector3 (hit.point.x, hammerPos.position.y, hit.point.z);
+			}
+		}
+
+		if (Input.GetMouseButtonDown(0)) {
+			hitAnimation.SetTrigger("PlayHit");
+		}
+	}
+
+	void OnCollisionEnter(Collision other) {
+		Debug.Log ("Hit");
+		if (other.transform.tag == "Sword") {
+			Debug.Log ("HitSword");
+			other.transform.GetComponent<Sword> ().HammerHit (5);
+		}
+	}
+}
---
+++ BlackSmithSim/Assets/Scripts/Sword.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour {
+
+	GameObject swordMeshBase;
+	GameObject swordMeshFinal;
+	SkinnedMeshRenderer skinnedMeshRenderer1;
+	SkinnedMeshRenderer skinnedMeshRenderer2;
+	//base blends
+	public float blendBot;
+	public float blendTop;
+
+	//final blends
+	public float blendFlat;
+	public float blendBevel;
+	public float blendSharp;
+
+	public bool finalMesh = false;
+
+	void Start() {
+		swordMeshBase = transform.GetChild (0).gameObject;
+		swordMeshFinal = transform.GetChild (1).gameObject;
+		swordMeshFinal.SetActive (false);
+		skinnedMeshRenderer1 = swordMeshBase.GetComponent<SkinnedMeshRenderer> ();
+		skinnedMeshRenderer2 = swordMeshFinal.GetComponent<SkinnedMeshRenderer> ();
+	}
+
+	public void HammerHit(float hitStrength, float localHitPostZ) {
+		if (!finalMesh) {
+			if (localHitPostZ <= -5) {
+				blendTop -= hitStrength;
+				skinnedMeshRenderer1.SetBlendShapeWeight (1, blendTop);
+			} else if (localHitPostZ >= 5) {
+				blendBot -= hitStrength;
+				skinnedMeshRenderer1.SetBlendShapeWeight (0, blendBot);
+			} else {
+				blendTop -= hitStrength;
+				blendBot -= hitStrength;
+				skinnedMeshRenderer1.SetBlendShapeWeight (1, blendTop);
+				skinnedMeshRenderer1.SetBlendShapeWeight (0, blendBot);
+			}
+			if (blendBot <= 0 && blendTop <= 0 && !finalMesh) {
+				swordMeshBase.SetActive (false);
+				swordMeshFinal.SetActive (true);
+				finalMesh = true;
+			}
+		} else {
+			blendFlat -= hitStrength;
+			skinnedMeshRenderer2.SetBlendShapeWeight (1, blendFlat);
+		}
+	}
+
+	public void HammerHit(float hitStrength) {
+		if (!finalMesh) {
+			blendTop -= hitStrength;
+			blendBot -= hitStrength;
+			skinnedMeshRenderer1.SetBlendShapeWeight (1, blendTop);
+			skinnedMeshRenderer1.SetBlendShapeWeight (0, blendBot);
+			if (blendBot <= 0 && blendTop <= 0 && !finalMesh) {
+				swordMeshBase.SetActive (false);
+				swordMeshFinal.SetActive (true);
+				finalMesh = true;
+			}
+		} else {
+			blendFlat -= hitStrength;
+			skinnedMeshRenderer2.SetBlendShapeWeight (1, blendFlat);
+		}
+	}
+}
